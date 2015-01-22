697fd0a423c5ed008133623b587bfadf8717aac2
Shuhei Nomura <nomura@prodigy-inc.co.jp>
2015-01-22 17:14:16 +0900

    Hello, world的な空っぽのプロジェクトを作成した。

---
+++ ProjectMC/MC.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace MC.Console
+{
+	/// <summary>
+	/// エントリ ポイント クラス。
+	/// </summary>
+	static class Program
+	{
+		/// <summary>
+		/// エントリ ポイント。
+		/// </summary>
+		private static void Main()
+		{
+			System.Console.WriteLine(@"何かキーを押すと終了します");
+			System.Console.Read();
+		}
+	}
+}

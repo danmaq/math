65b2e76f6478a325cbb6a97c04959e3698a08140
Shuhei Nomura <nomura@prodigy-inc.co.jp>
2015-01-30 19:06:53 +0900

    ログイン ゲーム フロー状態をガワだけ追加。 IOインターフェイスを追加。激しく仮なので今後別物になる可能性大。

---
+++ ProjectMC/MC.Core/Data/IOHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MC.Core.Data
+{
+	public static class IOHelper
+	{
+
+		public static Func<string, string> LoadFile
+		{
+			get;
+			set;
+		}
+
+		public static Func<string, string, Exception> SaveFile
+		{
+			get;
+			set;
+		}
+
+	}
+}

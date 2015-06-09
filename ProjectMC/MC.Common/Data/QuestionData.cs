11c955fb064c27c409fc1494a762fb1ee0e92590
danmaq <info@danmaq.com>
2015-06-10 00:20:24 +0900

    問題用構造体作成。

---
+++ ProjectMC/MC.Common/Data/QuestionData.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Collections.Generic;
+
+namespace MC.Common.Data
+{
+
+	/// <summary>
+	/// 問題データ。
+	/// </summary>
+	public struct QuestionData
+	{
+
+		/// <summary>
+		/// 問題。
+		/// </summary>
+		public string Question
+		{
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// 回答一覧。
+		/// </summary>
+		public IEnumerable<string> Answers
+		{
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// 有効期限。
+		/// </summary>
+		public int expires
+		{
+			get;
+			private set;
+		}
+	}
+}
---
+++ ProjectMC/MC.Common/Data/QuestionGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MC.Common.Data
+{
+	public sealed class QuestionGenerator
+	{
+
+		/// <summary>
+		/// シングルトン オブジェクト。
+		/// </summary>
+		public static QuestionGenerator Instance
+		{
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// コンストラクタ。
+		/// </summary>
+		private QuestionGenerator()
+		{
+		}
+
+		public IEnumerable<QuestionData> GetQuestion1_1()
+		{
+			var result = new List<QuestionData>();
+			const string question = @"物の数を数えましょう。";
+
+			for (int i = 10; --i >= 0;)
+			{
+				var item = new QuestionData() { };
+				result.Add(item);
+			}
+			return result;
+		}
+	}
+}

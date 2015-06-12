3ef71f3f3eb2dc4dc7427a8cef92dcd53e6369a3
Shuhei Nomura <nomura@prodigy-inc.co.jp>
2015-06-12 13:09:56 +0900

    まだ旧題(Project MC)が残っていたため対応

--- ProjectMC/MC.Console/Properties/AssemblyInfo.cs
+++ ProjectMC/MC.Console/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.*")]
-[assembly: AssemblyFileVersion("0.1.0.2")]
+[assembly: AssemblyFileVersion("0.1.0.4")]
 [assembly: NeutralResourcesLanguage("ja-JP")]
 

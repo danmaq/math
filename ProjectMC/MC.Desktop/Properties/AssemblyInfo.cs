452a9c305bfc1b47a0e0ebaea991ba14a414b462
danmaq <yasaimura@hotmail.com>
2015-08-18 21:10:58 +0900

    バージョン番号を最新に合わせた。

--- ProjectMC/MC.Desktop/Properties/AssemblyInfo.cs
+++ ProjectMC/MC.Desktop/Properties/AssemblyInfo.cs
@@ -52,7 +52,7 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.2.1.*")]
-[assembly: AssemblyFileVersion("0.2.1.72")]
+[assembly: AssemblyFileVersion("0.2.1.73")]
 [assembly: NeutralResourcesLanguage("ja-JP")]
 [assembly: Guid("c5d62caf-a7c0-445f-8fa8-aba6625fba6d")]
 
